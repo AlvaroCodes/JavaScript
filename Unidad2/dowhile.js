3ce9f545ab49a8072d7ae468c4e8016ac4ad9262
AlvaroCasa <aramirezm098@gmail.com>
2021-10-10 10:55:18 +0200

    He terminado el ejercicio de clase

--- Unidad2/dowhile.js
+++ Unidad2/dowhile.js
@@ -1,18 +1,18 @@
 function numeroPrimo (){
 
     let numero = document.getElementById('numeroPrimo').value;
-    let n = true;
+    let salida = false;
 
-        do {
-            for (let i = 2; i<=numero; i++){
-                if (numero%i == 0){
-                    alert("No es primo");
-                    numero++;
-                } else {
-                    alert("El "+ numero +" es primo");
-                    n = false;
-                    break;
-                }
+    do{
+        for (let i = 2; i<=numero; i++){
+            if (numero%i == 0){
+                break;
+            }else {
+                salida = true;
             }
-        } while (n == true);
+        }
+        numero ++;
+    }while(salida == false);
+        
+    alert(`El numero ${numero} es el siguiente primo`);
 }
