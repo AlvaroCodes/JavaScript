66edbab096b55280fa340f4a9a5ef10d8de935a1
Alvaro-Clase <aramirezm098@gmail.com>
2021-11-26 13:41:52 +0100

    Nuevas cosas de objetos aprendidas en clase

---
+++ Objetos/Window/navegator/navegator.js
@@ -0,0 +1,5 @@
+ if (!window.navigator.javaEnabled()){
+     alert("No esta activado");
+ }else{
+     alert("Esta activado");
+ }
---
+++ Objetos/Window/screen/app.js
@@ -0,0 +1,9 @@
+let diferencia = screen.height - screen.availHeight
+console.log(diferencia);
+
+let tamañoPantalla2 = screen.height * screen.width;
+let tamañoPantalla = screen.availHeight * screen.availWidth;
+console.log(tamañoPantalla);
+console.log(tamañoPantalla2);
+
+console.log(tamañoPantalla2-tamañoPantalla);
